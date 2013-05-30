ddbd6ae253348e43a034baa03c0919a606d1341f
Leo <lmartel@stanford.edu>
2013-05-30 03:16:59 -0700

    switched extension on test results to be spimmable

--- run_tests.sh
+++ run_tests.sh
@@ -1,13 +1,13 @@
-rm test/*.s.ref -f
-rm test/*.s.our -f
+rm test/*.ref.s -f
+rm test/*.our.s -f
 mv test/*.cl .
 # FILES="*.cl"
 FILES="hello_world.cl"
 for f in $FILES
 do
   /usr/class/cs143/bin/coolc $f
-  mv *.s test/$f.s.ref
+  mv *.s test/$f.ref.s
   ./mycoolc $f
-  mv *.s test/$f.s.our
+  mv *.s test/$f.our.s
 done
 mv *.cl test/
